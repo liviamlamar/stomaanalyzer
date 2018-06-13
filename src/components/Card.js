65ec694705452641e23099a0c87645c32a164454
liviamlamar <31834459+liviamlamar@users.noreply.github.com>
2018-06-13 12:31:29 -0400

    adiçao de props para modal

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
 const Card = (props) => {
-    console.log(props.endereco)
+    // console.log(props.endereco)
     return (
         <div key={props.chave} className="col-mb-3" style={{ marginRight: '10px' }}>
             <div className="card" style={{ maxWidth: "18rem", marginBottom: "10px" }}>
-                <img src={props.endereco} alt='imagem' style={{ width: "250px", height: "200px" }} />
+                <img src={props.endereco} alt='imagem' style={{ width: "250px", height: "200px" }} data-toggle={props.dataToggle} data-target={props.dataTarget} onClick={props.funcao}/>
             </div>
         </div>
     )
